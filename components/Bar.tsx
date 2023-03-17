455def027f106a0073129b789639842ce2a4bf52
Tri Nguyen Van <nguyenvantri2820@gmail.com>
2023-03-18 02:41:47 +0700

    feat: login screen

--- components/Bar.tsx
+++ components/Bar.tsx
@@ -1,14 +1,15 @@
 import * as React from 'react';
 import { useEffect, useRef, useState } from 'react';
-import { Dimensions, Keyboard, KeyboardAvoidingView, StyleSheet, Pressable, SafeAreaView, Text, View, TouchableOpacity, Linking, Platform } from 'react-native';
+import { Dimensions, Keyboard, KeyboardAvoidingView, StyleSheet, Pressable, SafeAreaView, Text, Image, View, TouchableOpacity, Linking, Platform, ImageBackground } from 'react-native';
 import { SafeAreaInsetsContext, useSafeAreaInsets } from 'react-native-safe-area-context';
 import { constants, normalizedHostname } from '../utils';
-import Animated, { Easing, FadeInDown, FadeOutDown, FadeOutUp, KeyboardState, useAnimatedKeyboard, useAnimatedReaction, useAnimatedStyle, useDerivedValue, useSharedValue, withSpring, withTiming } from 'react-native-reanimated';
+import Animated, { Easing, FadeIn, FadeInDown, FadeOut, FadeOutDown, FadeOutUp, KeyboardState, useAnimatedKeyboard, useAnimatedReaction, useAnimatedStyle, useDerivedValue, useSharedValue, withSpring, withTiming } from 'react-native-reanimated';
 import { Gesture, GestureDetector, TextInput } from 'react-native-gesture-handler';
 import { signIn } from '../login';
 import { supabaseClient, upsertProfile } from '../supabaseClient';
 import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
 import Ionicons from '@expo/vector-icons/Ionicons';
+import { LinearGradient } from 'expo-linear-gradient';
 
 
 function Bar(props: any) {
@@ -34,6 +35,12 @@ function Bar(props: any) {
             transform: [
                 { scale: props.offset.value > 1 ? (2 - 1 / Math.pow(props.offset.value, 0.2)) : 1 },
             ]
+        };
+    });
+
+    const loginStyles = useAnimatedStyle(() => {
+        return {
+            opacity: Math.pow(offset.value / minOffset, 0.3)
         };
     });
 
@@ -186,6 +193,8 @@ function Bar(props: any) {
                     backgroundColor: props.mode.tag == 'Comment' ? '#212121' : '#151316',
                     borderTopWidth: StyleSheet.hairlineWidth,
                     borderTopColor: '#2A2829',
+                    borderTopLeftRadius: 4,
+                    borderTopRightRadius: 4,
                 }, barStyles]}>
 
                     {/* HANDLER */}
@@ -351,21 +360,68 @@ function Bar(props: any) {
 
                     </Animated.View>
 
+                    <Animated.View
+                        style={loginStyles}
+                    >
+                        <ImageBackground
+                            style={{
+                                position: 'absolute',
+                                top: 4,
+                                width: constants.width,
+                                height: 600,
+                            }}
+                            source={require('../assets/loginBackground.jpg')}
+                        >
+                            <LinearGradient colors={['transparent', props.mode.tag == 'Comment' ? '#212121' : '#151316']} style={{
+                                width: '100%',
+                                height: '100%'
+                            }}
+                                pointerEvents='none'
+                            />
+                        </ImageBackground>
 
-                    <TouchableOpacity onPress={() => {
-                        signInAndUpdateProfile('apple')
-                    }}>
+
+                        <Image
+                            style={{
+                                marginTop: 100,
+                                width: 60,
+                                height: 60,
+                                borderRadius: 4,
+                                alignSelf: 'center'
+                            }}
+                            source={require('../assets/icon.png')}
+                        />
+
+                        <Text style={{
+                            marginTop: 24,
+                            color: 'white',
+                            fontSize: 30,
+                            fontWeight: '700',
+                            alignSelf: 'center',
+                            textAlign: 'center'
+                        }}>Explore the Internet. Powered by Packer.</Text>
+
                         <View style={{
-                            height: 30,
-                            width: '100%',
-                            backgroundColor: 'blue'
-                        }} >
-                            {/* <IconBrandApple size={28} floodColor='red' color='yellow' /> */}
-                            <FontAwesome5 name='facebook' backgroundColor="#3b5998" onPress={() => { }}>
-                                Login with Facebook
-                            </FontAwesome5>
+                            marginTop: 32,
+                            marginLeft: 'auto',
+                            marginRight: 'auto',
+                            shadowColor: 'black',
+                            shadowOpacity: 0.8,
+                            shadowOffset: { width: 0, height: 2 },
+                            shadowRadius: 4,
+                            elevation: 3,
+                        }}>
+                            <Ionicons.Button name='logo-apple' style={{
+                                backgroundColor: "black",
+                                paddingVertical: 10,
+                                paddingHorizontal: 32,
+                            }} onPress={() => {
+                                signInAndUpdateProfile('apple')
+                            }}>
+                                Sign in with Apple
+                            </Ionicons.Button>
                         </View>
-                    </TouchableOpacity>
+                    </Animated.View>
 
 
                 </Animated.View>
