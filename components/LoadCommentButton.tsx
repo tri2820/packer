f841f89ca28603c8eeecf68f2076cac490aa29c0
Tri Nguyen Van <nguyenvantri2820@gmail.com>
2023-04-02 07:05:09 +0700

    feat: loading

--- components/LoadCommentButton.tsx
+++ components/LoadCommentButton.tsx
@@ -1,29 +1,57 @@
 import * as React from 'react';
-import { memo } from 'react';
-import { Text, TouchableOpacity } from 'react-native';
+import { memo, useState } from 'react';
+import { Text, TouchableOpacity, StyleSheet, ActivityIndicator } from 'react-native';
+
 
 function LoadCommentButton(props: any) {
-    return <TouchableOpacity style={{
+    const [visible, setVisible] = useState(true);
+
+    const load = () => {
+        setVisible(false);
+        props.requestComments(props.post_id, props.ofId);
+    }
+
+    return visible
+        ? <TouchableOpacity style={
+            [
+                {
+                    marginLeft: props.level <= 1 ? 0 : (16 * props.level) + props.level == 0 ? 0 : (props.level == 1 ? 2 : 18),
+                },
+                styles.button
+            ]}
+            onPress={load}
+        >
+            <Text style={styles.text}>
+                Load {props.num} more
+            </Text>
+        </TouchableOpacity>
+        :
+        <ActivityIndicator
+            style={styles.loading_indicator}
+            size="small"
+            color="#6b5920" />
+}
+
+export default LoadCommentButton;
+export const MemoLoadCommentButton = memo(LoadCommentButton);
+
+const styles = StyleSheet.create({
+    button: {
         backgroundColor: '#2C2C2C',
         marginBottom: 8,
         alignSelf: 'center',
         paddingVertical: 8,
         paddingHorizontal: 16,
-        marginLeft: props.level <= 1 ? 0 : (16 * props.level) + props.level == 0 ? 0 : (props.level == 1 ? 2 : 18),
         borderRadius: 4
-    }}
-        onPress={async () => {
-            await props.requestComments(props.post_id, props.ofId);
-        }}
-    >
-        <Text style={{
-            color: '#e6e6e6',
-            fontWeight: '500'
-        }}>
-            Load {props.num} more
-        </Text>
-    </TouchableOpacity>
-}
-
-export default LoadCommentButton;
-export const MemoLoadCommentButton = memo(LoadCommentButton);
+    },
+    text: {
+        color: '#e6e6e6',
+        fontWeight: '500'
+    },
+    loading_indicator: {
+        marginBottom: 8,
+        alignSelf: 'center',
+        paddingVertical: 8,
+        paddingHorizontal: 16
+    }
+})
