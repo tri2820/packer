8bded72c5723fcb0af65ae22637fa5431cdd7e26
Tri Nguyen Van <nguyenvantri2820@gmail.com>
2023-04-07 20:46:03 +0700

    fix

--- components/InputSend.tsx
+++ components/InputSend.tsx
@@ -18,6 +18,7 @@ function InputSend(props: any) {
 
     useEffect(() => {
         setText('');
+        props.setSelectedComment(null)
     }, [props.activePostIndex])
 
     const send = async () => {
@@ -25,6 +26,7 @@ function InputSend(props: any) {
             props.onSubmit(text, props.selectedComment);
         }
         setText('');
+        props.setSelectedComment(null)
         props.inputref.current?.blur();
     }
 
